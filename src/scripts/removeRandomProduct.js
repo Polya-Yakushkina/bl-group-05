836d84bc1b56e3263519ceafa66de1d54026c484
Vitalic <0makovskyi@gmail.com>
2024-10-19 12:29:35 +0300

    remove product

---
+++ src/scripts/removeRandomProduct.js
@@ -0,0 +1,21 @@
+import { PATH_DB } from '../constants/products.js';
+import fs from 'node:fs/promises';
+
+const removeRandomProduct = async () => {
+  const products = await fs.readFile(PATH_DB, 'utf-8');
+  const productData = JSON.parse(products);
+
+  if (productData.length === 0) {
+    return;
+  }
+
+  const randomIndex = Math.floor(Math.random() * productData.length);
+
+  const removeProduct = productData.splice(randomIndex, 1);
+
+  fs.writeFile(PATH_DB, JSON.stringify(productData, null, 2), 'utf-8');
+
+  return removeProduct;
+};
+
+console.log(await removeRandomProduct());
